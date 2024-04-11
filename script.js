7b658b86cbc1854272d05b4473b4eb7b65e1533b
Karl Young <Karlyoung112@gmail.com>
2024-04-11 19:30:18 +1200

    Spirals

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onload = function () {
 }
 
 window.addEventListener('resize', function () {
-  this.cancelAnimationFrame(flowFieldAnimation)
+  cancelAnimationFrame(flowFieldAnimation)
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
   flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height)
@@ -36,19 +36,43 @@ class FlowFieldEffect {
   constructor(ctx, width, height) {
     this.#ctx = ctx
     this.#ctx.strokeStyle = 'white'
-    this.#ctx.lineWidth = 5
+    this.#ctx.lineWidth = 1
     this.#width = width
     this.#height = height
     this.lastTime = 0
-    this.interval = 1000/60
+    this.interval = 1000 / 60
     this.timer = 0
-    this.cellSize = 15
+    this.cellSize = 10
+    this.gradient
+    this.#createGradient()
+    this.#ctx.strokeStyle = this.gradient
+    this.radius = 5
+    this.vr = 0.03
   }
-  #drawLine(x, y) {
-    const length = 300
+  #createGradient() {
+    this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, 0)
+    this.gradient.addColorStop('0.1', '#ff5c33')
+    this.gradient.addColorStop('0.2', '#ffff33')
+    this.gradient.addColorStop('0.4', '#ccccff')
+    this.gradient.addColorStop('0.6', '#b3ffff')
+    this.gradient.addColorStop('0.8', '#80ff80')
+    this.gradient.addColorStop('0.9', '#00beef')
+  }
+  #drawLine(angle, x, y) {
+    let positionX = x
+    let positionY = y
+    let dx = mouse.x - positionX
+    let dy = mouse.y - positionY
+    let distance = dx * dx + dy * dy
+    let length = distance / 10000
+    if (distance > 500000) {
+      distance = 500000
+    } else if (distance < 10000) {
+      distance = 10000
+    }
     this.#ctx.beginPath()
     this.#ctx.moveTo(x, y)
-    this.#ctx.lineTo(mouse.x, mouse.y)
+    this.#ctx.lineTo(x + Math.cos(angle) * length, y + Math.sin(angle) * length)
     this.#ctx.stroke()
   }
   animate(timeStamp) {
@@ -56,11 +80,23 @@ class FlowFieldEffect {
     this.lastTime = timeStamp
     if (this.timer > this.interval) {
       this.#ctx.clearRect(0, 0, this.#width, this.#height)
+      this.radius += this.vr
+
+      if (this.radius > 5 || this.vr < -5) {
+        this.vr *= -1
+        console.log(
+          'Reversing direction:',
+          'radius:',
+          this.radius,
+          'vr:',
+          this.vr
+        )
+      }
 
       for (let y = 0; y < this.#height; y += this.cellSize) {
-        for(let x = 0; x < this.#width; x += this.cellSize) {
-          this.#drawLine(this.#width / 2, this.#height / 2)
-
+        for (let x = 0; x < this.#width; x += this.cellSize) {
+          const angle = (Math.cos(x * 0.01) + Math.sin(y * 0.01)) * this.radius
+          this.#drawLine(angle, x, y)
         }
       }
 
